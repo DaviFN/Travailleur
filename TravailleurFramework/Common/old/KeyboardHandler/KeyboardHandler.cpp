32a67557dd144ce187fa05f9db3efe9a66f5cf3c
Davi Feliciano Nonnenmacher <davifn1@hotmail.com>
2025-04-26 22:43:40 -0300

    removing unnecessary stuff

--- TravailleurFramework/Common/old/KeyboardHandler/KeyboardHandler.cpp
+++
@@ -1,9 +0,0 @@
-#include "KeyboardHandler.h"
-
-KeyboardHandler::KeyboardHandler()
-{
-    for(int i = 0 ; i < 256 ; i++)
-    {
-        keyStates[i] = KEY_STATE_UNPRESSED;
-    }
-}
--- TravailleurFramework/Common/old/KeyboardHandler/KeyboardHandler_fwd.h
+++
@@ -1,6 +0,0 @@
-#ifndef KEYBOARD_HANDLER_FWD_H
-#define KEYBOARD_HANDLER_FWD_H
-
-class KeyboardHandler;
-
-#endif
